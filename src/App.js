f1ebb39b442130bbe4827294d8811366b5cad298
Jules Cayrol <julescayrol@gmail.com>
2024-02-01 11:31:05 +0100

    nouveau test url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-
 import Accueil from './pages/Accueil';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/Portfolio' element={<Accueil />} />
+      <Route path='/' element={<Accueil />} />
     </Routes>
   );
 };
