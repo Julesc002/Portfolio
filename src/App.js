b66f47d888d15c2c052fa2043a34b399fad34545
Jules Cayrol <julescayrol@gmail.com>
2024-02-01 11:36:29 +0100

    encore un test url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Accueil from './pages/Accueil';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Accueil />} />
+      <Route
+        path='/'
+        element={<Navigate to='/Julesc002/Portfolio' replace />}
+      />
+      <Route path='/Julesc002/Portfolio' element={<Accueil />} />
     </Routes>
   );
 };
