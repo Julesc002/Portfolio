ab048e2b7b13450bdffc1be855f1b759d4e4bcf4
Jules Cayrol <julescayrol@gmail.com>
2024-02-01 11:42:47 +0100

    test url du désespoir

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Accueil from './pages/Accueil';
 
 const App = () => {
   return (
     <Routes>
-      <Route
-        path='/'
-        element={<Navigate to='/Julesc002/Portfolio' replace />}
-      />
-      <Route path='/Julesc002/Portfolio' element={<Accueil />} />
+      <Route path='*' element={<Accueil />} />
     </Routes>
   );
 };
