5b424352bfb87de9a6c0302049de185993a066d2
Jules Cayrol <julescayrol@gmail.com>
2024-02-05 16:43:54 +0100

    changement de page fonctionnel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Accueil from './pages/Accueil';
+import AccueilBis from './pages/AccueilBis';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='*' element={<Accueil />} />
+      <Route path='/' element={<Accueil />} />
+      <Route path='/feur' element={<AccueilBis />} />
     </Routes>
   );
 };
---
+++ src/pages/AccueilBis.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AccueilBis = () => {
+    return (
+        <div>
+            <h1>Page accueil bis ratio ahah</h1>
+        </div>
+    );
+};
+
+export default AccueilBis;
