0d5c66ba3e0ed4885d6d4bad0dcd3be09c229b03
Jules Cayrol <julescayrol@gmail.com>
2024-02-06 10:18:28 +0100

    video CV

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 import { NavLink } from 'react-router-dom';
 
 const Accueil = () => {
@@ -8,6 +9,7 @@ const Accueil = () => {
             <NavLink to="/feur">
                     <button>go to feur</button>
             </NavLink>
+            <ReactPlayer url="https://youtu.be/1qVTgh8bMxU?si=BAA1aBP7SiPhLoP3" style={{ width: '100%', height: '100%' }} playing={true} />
         </div>
     );
 };
