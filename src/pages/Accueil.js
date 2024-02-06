2bfc96ce90adb2621f055a3619daa35ebbabe7f9
Jules Cayrol <julescayrol@gmail.com>
2024-02-06 09:08:59 +0100

    musique de fond

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Accueil = () => {
     return (
         <div>
             <h1>Page accueil</h1>
+            <NavLink to="/feur">
+                    <button>go to feur</button>
+            </NavLink>
         </div>
     );
 };
--- src/pages/AccueilBis.js
+++ src/pages/AccueilBis.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import audio from '../audio/audio.mp3';
 
 const AccueilBis = () => {
+
+    const executed = useRef(false);
+
+    useEffect(() => {
+        if (!executed.current) {
+            new Audio(audio).play();
+            executed.current = true;
+        }
+    }, []);
+
     return (
         <div>
             <h1>Page accueil bis ratio ahah</h1>
