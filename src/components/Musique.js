72886fd31e12ffedd7f677994a4c03c598c0e646
Jules Cayrol <julescayrol@gmail.com>
2024-02-08 16:55:24 +0100

    fadein et out de la musique

--- src/components/Musique.js
+++ src/components/Musique.js
@@ -1,36 +1,72 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import tunic1 from '../audio/tunic1.mp3';
 import tunic2 from '../audio/tunic2.mp3';
 
 const Musique = () => {
     const location = useLocation();
+    const [currentLocation, setCurrentLocation] = useState(location);
     const executed = useRef(false);
     const audio1 = useRef(new Audio(tunic1));
     const audio2 = useRef(new Audio(tunic2));
 
+    const fadeInOutDuration = 2000;
+
+    const fadeInOut = (audioIn, audioOut) => {
+        const interval = 100; // Intervalle entre chaque augmentation de volume
+        const steps = fadeInOutDuration / interval; // Nombre d'étapes
+        const deltaVolume = 1 / steps; // Variation de volume à chaque étape
+
+        let currentStep = 0;
+
+        const fadeFunction = () => {
+            if (currentStep < steps) {
+                if (audioIn.current.volume < 0.5) {
+                    audioIn.current.volume = Math.min(0.5, audioIn.current.volume + deltaVolume);
+                }
+                if (audioOut.current.volume > 0) {
+                    audioOut.current.volume = Math.max(0, audioOut.current.volume - deltaVolume);
+                }
+                currentStep++;
+                setTimeout(fadeFunction, interval);
+            }
+        };
+        
+
+        fadeFunction();
+    };
+
     useEffect(() => {
         if (!executed.current) {
             audio2.current.volume = 0;
+            audio1.current.volume = 0;
+            audio1.current.loop = true;
+            audio2.current.loop = true;
             audio1.current.play();
             audio2.current.play();
+            fadeInOut(audio1, audio2);
             executed.current = true;
         }
     }, []);
 
     useEffect(() => {
-        if (executed.current) {
-            if (audio1.current.volume === 1) {
-                audio1.current.volume = 0;
-                audio2.current.volume = 1;
+        if (currentLocation !== location) {
+            if (audio1.current.volume > 0.1) {
+                fadeInOut(audio2, audio1);
             } else {
-                audio1.current.volume = 1;
-                audio2.current.volume = 0;
+                fadeInOut(audio1, audio2);
             }
+            setCurrentLocation(location);
         }
-    }, [location]);
+        //console.log("1 : " + audio1.current.volume);
+        //console.log("2 : " + audio2.current.volume);
+    }, [location, currentLocation]);
 
-    return <></>;
+    return (
+        <div>
+           
+        </div>
+    );
 };
 
 export default Musique;
