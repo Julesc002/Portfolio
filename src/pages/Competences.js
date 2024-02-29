fd87ec6b844c63de318ccd4c0743491e164b9e89
Jules Cayrol <julescayrol@gmail.com>
2024-02-29 10:06:53 +0100

    fin page projet et page competences

--- src/pages/Competences.js
+++ src/pages/Competences.js
@@ -6,6 +6,66 @@ function Competences() {
         <div>
             <Header />
             <h1>Page compétences</h1>
+            <div className='containerCompetence'>
+                <div className='realiser'>
+                    <h3>Réaliser</h3>
+                    <p>Je suis capable de concevoir, coder et tester une application</p>
+                    <li>
+                        Travaux notables :
+                        <ul>Ecocab (voir page projet)</ul>
+                        <ul>WekinGames (voir page projet)</ul>
+                        <ul>Gestus 2000 (voir page projet)</ul>
+                    </li>
+                </div>
+                <div className='optimiser'>
+                    <h3>Optimiser</h3>
+                    <p>Je suis capable d'optimiser une application pour réduire son temps d'exécution et sa consommation de resources</p>
+                    <li>
+                        Travaux notables :
+                        <ul>Ecocab (voir page projet)</ul>
+                        <ul>WekinGames (voir page projet)</ul>
+                        <ul>Gestus 2000 (voir page projet)</ul>
+                    </li>
+                </div>
+                <div className='collaborer'>
+                    <h3>Collaborer</h3>
+                    <p>Je suis capable de travailler efficacement dans une équipe informatique</p>
+                    <li>
+                        Travaux notables :
+                        <ul>Ecocab (voir page projet) - Travail dans une équipe de 2</ul>
+                        <ul>WekinGames (voir page projet) - Travail dans une équipe de 6</ul>
+                        <ul>Gestus 2000 (voir page projet) - Travail dans une équipe de 2</ul>
+                    </li>
+                </div>
+                <div className='gerer'>
+                    <h3>Gérer</h3>
+                    <p>Je suis capable de concevoir, gérer et exploiter des données stockées dans une base de données</p>
+                    <li>
+                        Travaux notables :
+                        <ul>Ecocab (voir page projet) - Utilisation d'une base de données MongoDB</ul>
+                        <ul>WekinGames (voir page projet) - Utilisation d'une base de données MongoDB</ul>
+                        <ul>Gestus 2000 (voir page projet) - Utilisation d'une base de données MySQL</ul>
+                    </li>
+                </div>
+                <div className='conduire'>
+                    <h3>Conduire</h3>
+                    <p>Je suis capable d'organiser et piloter un projet informatique avec des méthodes agiles</p>
+                    <li>
+                        Travaux notables :
+                        <ul>Ecocab (voir page projet) - Utilisation de la méthode agile à l'aide de Trello</ul>
+                        <ul>WekinGames (voir page projet) - Utilisation de la méthode agile à l'aide de GitLab board</ul>
+                    </li>
+                </div>
+                <div className='administrer'>
+                    <h3>Administrer</h3>
+                    <p>Je suis capable d'installer, configurer et maintenir des services informatiques</p>
+                    <li>
+                        Travaux notables :
+                        <ul>Ecocab (voir page projet) - Sécurisation d'une base de données MongoDB</ul>
+                        <ul>WekinGames (voir page projet) - Sécurisation d'une base de données MongoDB</ul>
+                    </li>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/Projets.js
+++ src/pages/Projets.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import ecocab from '../ecocab.png';
+import quidditch from '../quidditch.png';
 import wekingame from '../wekingame.png';
 
 function Projets() {
@@ -8,15 +9,23 @@ function Projets() {
         <div>
             <Header />
             <h1>Projets</h1>
-            <div className='Ecocab'>
-                <h3>Stage : Ecocab - Développement d'un MVP d'une application de covoiturage</h3>
-                <img className='image_ecocab' src={ecocab} alt="ecocab" />
-                <p>Code disponible sur mon <a href="https://github.com/Julesc002/Stage-Ecocab">Github</a></p>
-            </div>
-            <div className='Wekingame'>
-                <h3>Projet de groupe : WekinGame - Site de gestion de wiki sur les jeux vidéos</h3>
-                <img className='image_wekingame' src={wekingame} alt="wekingame" />
-                <p>Code disponible sur mon <a href="https://github.com/Julesc002/WekinGame">Github</a></p>
+            <div className='containerProjet'>
+                <div className='Ecocab'>
+                    <h3>Stage : Ecocab - Développement d'un MVP d'une application de covoiturage</h3>
+                    <img className='image_ecocab' src={ecocab} alt="ecocab" />
+                    <p>Code disponible sur mon <a href="https://github.com/Julesc002/Stage-Ecocab">Github</a></p>
+                </div>
+                <div className='Wekingame'>
+                    <h3>Projet de groupe : WekinGame - Site de gestion de wiki sur les jeux vidéos</h3>
+                    <img className='image_wekingame' src={wekingame} alt="wekingame" />
+                    <p>Code disponible sur mon <a href="https://github.com/Julesc002/WekinGame">Github</a></p>
+                </div>
+                <div className='Quidditch'>
+                    <h3>Projet de groupe : Gestus 2000 - Site de gestion d'équipe de Quidditch</h3>
+                    <img className='image_quidditch' src={quidditch} alt="quidditch" />
+                    <p>Ce site est hébergé <a href="https://projetphpquidditch.000webhostapp.com/">içi</a></p>
+                    <p>Vous pouvez vous connecter avec le login "guest" et le mot de passe "1234"</p>
+                </div>
             </div>
         </div>
     );
